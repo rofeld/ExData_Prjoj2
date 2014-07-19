9a337a991f139cfa247d2c9d753aec79838bce7d
rofeld <rofeld.mailbox@gmail.com>
2014-07-18 20:55:45 -0400

    first commit

---
+++ plot6.R
@@ -0,0 +1,24 @@
+cur_dir = "C:/dev/R/Coursera/Proj2" #working directory where .rds files are located
+setwd(cur_dir)
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+motor_indx <- SCC[grep("vehicles", SCC$"Short.Name", ignore.case=T),"SCC"]
+
+NEI.Baltimore.LA <- subset(NEI, (fips == "24510" | fips == "06037") & SCC %in% motor_indx) # Baltimore City, Maryland (fips == "24510")
+
+df.tot.emiss <- aggregate(NEI.Baltimore.LA[, "Emissions"], list(Year = NEI.Baltimore.LA$year, fips = NEI.Baltimore.LA$fips), FUN = "sum")
+
+df.tot.emiss["city"] <- gsub("06037", "LA", gsub("24510", "Baltimore", df.tot.emiss$fips))
+
+library(ggplot2)
+library(scales)
+gg <- ggplot(df.tot.emiss, aes(x = Year, y = x, col = city)) + geom_line() + scale_y_continuous(name="Total Emissions", labels = comma) 
+gg <- gg + scale_color_discrete(name = "Location")
+gg <- gg + ggtitle("Motor Vehicle Sources")
+#print(gg)
+
+dev.copy(png,'plot6.png', width = 480, height = 480)
+dev.off()
